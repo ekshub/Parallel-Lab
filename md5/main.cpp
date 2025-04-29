b5cdfe75ca40c96de1a58b2ce43fb89b60025e3e
ekshub <155219331+ekshub@users.noreply.github.com>
2025-04-30 01:39:10 +0800

    Add files via upload

--- md5/main.cpp
+++ md5/main.cpp
@@ -1,40 +1,62 @@
-#include "md5_2x.h"
+#include "md5_8x.h"//仅包含你所使用的并行度的头文件
+
 #include<iostream>
 #include<iomanip>
 #include <fstream>
 #include <chrono>
 using namespace std;
+#define  Parallel_Level 8   //修改以适应并行度，后面的代码会自动适应
+
+void MD5HashBatch8(const vector<string>& inputs, vector<bit32*>& states);
+void MD5HashBatch4(const vector<string>& inputs, vector<bit32*>& states);
+void MD5HashBatch2(const vector<string>& inputs, vector<bit32*>& states);
+void MD5Hash(const string& input, bit32* state);
 // 返回处理耗时（毫秒）
 double process_batch(std::vector<std::string>& guesses) {
-    std::vector<bit32*> states(2);
-    for (int j = 0; j < 2; ++j) {
+    std::vector<bit32*> states(Parallel_Level);
+    for (int j = 0; j < Parallel_Level; ++j) {
         states[j] = new bit32[4];
     }
 
-    std::vector<std::string> pw_arr(2, "");
+    std::vector<std::string> pw_arr(Parallel_Level, "");
     size_t total = guesses.size();
-    size_t full_batches = total / 2;
-    size_t remainder = total % 2;
+    size_t full_batches = total / Parallel_Level;
+    size_t remainder = total % Parallel_Level;
 
     auto start = std::chrono::high_resolution_clock::now();
 
     for (size_t b = 0; b < full_batches; ++b) {
-        size_t base = b * 2;
-        for (int k = 0; k < 2; ++k) {
+        size_t base = b * Parallel_Level;
+        for (int k = 0; k < Parallel_Level; ++k) {
             pw_arr[k] = guesses[base + k];
         }
-        MD5HashBatch2(pw_arr, states);
+        if(Parallel_Level == 8)
+            MD5HashBatch8(pw_arr, states);
+        else if (Parallel_Level == 4)
+            MD5HashBatch4(pw_arr, states);
+        else if (Parallel_Level == 2)
+            MD5HashBatch2(pw_arr, states);
+        else
+            MD5Hash(pw_arr[0], states[0]);
+       
     }
 
     if (remainder > 0) {
-        size_t base = full_batches * 2;
+        size_t base = full_batches * Parallel_Level;
         for (size_t k = 0; k < remainder; ++k) {
             pw_arr[k] = guesses[base + k];
         }
-        for (size_t k = remainder; k < 2; ++k) {
+        for (size_t k = remainder; k < Parallel_Level; ++k) {
             pw_arr[k] = pw_arr[0]; // 重复第一个填充
         }
-        MD5HashBatch2(pw_arr, states);
+         if(Parallel_Level == 8)
+            MD5HashBatch8(pw_arr, states);
+        else if (Parallel_Level == 4)
+            MD5HashBatch4(pw_arr, states);
+        else if (Parallel_Level == 2)
+            MD5HashBatch2(pw_arr, states);
+        else
+            MD5Hash(pw_arr[0], states[0]);
     }
 
     auto end = std::chrono::high_resolution_clock::now();
